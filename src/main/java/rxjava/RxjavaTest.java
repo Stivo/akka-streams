486a03a8cd21f5b1df33da0ca7b01213c18d94ba
Stefan Ackermann <stivo@gmx.ch>
2018-01-16 22:56:30 +0100

    Actually back up a whole tree

--- src/main/java/rxjava/RxjavaTest.java
+++ src/main/java/rxjava/RxjavaTest.java
@@ -4,32 +4,55 @@ import akka.util.ByteString;
 import backupper.BackupStreams;
 import backupper.model.*;
 import com.github.davidmoten.rx2.Bytes;
+import com.github.davidmoten.rx2.Flowables;
+import io.reactivex.CompletableSource;
 import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.flowables.ConnectableFlowable;
 import io.reactivex.parallel.ParallelFlowable;
 import io.reactivex.schedulers.Schedulers;
 import org.apache.commons.io.FileUtils;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static io.reactivex.BackpressureStrategy.BUFFER;
 
 public class RxjavaTest {
     public static void main(String[] args) throws Exception {
+        System.setProperty("rx2.computation-threads", "8");
         File backup = new File("backup");
         FileUtils.deleteDirectory(backup);
         backup.mkdir();
-        System.setProperty("rx2.computation-threads", "8");
+        List<Path> toBackup = Files.walk(Paths.get("restored"))
+                .filter(Files::isRegularFile)
+                .filter(e -> !e.toAbsolutePath().toString().contains(".git"))
+                .collect(Collectors.toList());
+        CompletableFuture<Boolean> done = new CompletableFuture<>();
+//        toBackup.parallelStream()
+//                .flatMap(e -> Stream.of(backupFile(e.toFile())))
+//                .forEach(e -> e.join());
+        Flowable.fromIterable(toBackup)
+                .parallel(10)
+                .map(e -> backupFile(e.toFile()))
+                .sequential().forEach(e -> e.join());
+//        booleanParallelFlowable.sequential().forEach(e -> )
+//                .forEach(e -> e.join());
+        BackupStreams.shutdown();
+    }
+
+    private static CompletableFuture<Boolean> backupFile(File file) {
         Iterable<Integer> naturals = IntStream.iterate(0, i -> i + 1)::iterator;
-        File file = new File("restored/IMG_0001.JPG");
         Flowable<byte[]> from = Bytes.from(file, 100000);
 
         CompletableFuture<Boolean> future1 = new CompletableFuture<>();
@@ -39,21 +62,12 @@ public class RxjavaTest {
             FileDescription fileDescription = new FileDescription(file);
             BlockId blockId = new BlockId(fileDescription, i);
             Block block = new Block(blockId, ByteString.fromArray(s), null);
-//            State state = new State();
-//            state.byteBuffer = ByteBuffer.wrap(s);
-//            state.number = i;
-//            state.file = file;
-//            return state;
             return block;
         });
         ParallelFlowable<Block> parallelBlocks = blockFlowable.flatMap(x -> Observable.just(x).toFlowable(BUFFER))
                 .parallel(10)
                 .runOn(Schedulers.computation(), 100);
 
-        LoggingSubscriber<Block> blockLoggingSubscriber = new LoggingSubscriber<>();
-        Subscriber[] subscribers = {blockLoggingSubscriber};
-//        parallelBlocks.subscribe(subscribers);
-//        parallelBlocks.sequential().forEach(e -> System.out.println(e));
         ConnectableFlowable<Block> md51 = parallelBlocks
                 .flatMap(block -> {
                     MessageDigest messageDigest = BackupStreams.config().newHasher();
@@ -70,10 +84,7 @@ public class RxjavaTest {
                     }
                     return e;
                 }).sequential(100).publish();
-//        md51.sequential().forEach()
-//        ParallelFlowable<Block> md511 = md51;
-//        md511.subscribe(subscribers);
-//
+
         md51.toSortedList(Comparator.comparingInt(b -> b.blockId().blockNr()))
                 .flatMapObservable(e -> {
                     int hashLength = BackupStreams.config().hashLength();
@@ -88,6 +99,7 @@ public class RxjavaTest {
 
                     return Observable.fromFuture(future);
                 }).subscribe(e -> {}, e -> {e.printStackTrace();}, () -> future2.complete(true));
+
         md51.parallel(10).runOn(Schedulers.computation())
                 .flatMap(block -> {
                     block.compress(BackupStreams.config());
@@ -105,73 +117,8 @@ public class RxjavaTest {
                 }
                 ).sequential().subscribe(e -> {}, e -> {e.printStackTrace();}, () -> future1.complete(true));
         md51.connect();
-//        md51.sequential().forEach(e -> System.out.println(e));
-//        hashedBlocks.parallel().filter(e -> e.isAlreadySaved()).map(e -> {
-//                    System.out.println("Should save " + e.blockId());
-//                    return null;
-//                }
-//        ).reduce(() -> 1, (e, f) -> e).runOn(Schedulers.computation()).sequential().forEach( e-> future1.complete(true));
 
-//        Flowable<List<Block>> map = hashedBlocks.parallel().toSortedList(Comparator.comparingInt(e -> e.blockId().blockNr()))
-//                .map(e -> {
-//                    System.out.println("Got hashlist for " + e.size() + " blocks");
-//                    future2.complete(true);
-//                    return e;
-//                });
-//
-//        flowToSave.runOn(Schedulers.computation())
-//                .sequential()
-//                .subscribe(e -> System.out.println(e+""), e -> e.printStackTrace(), () -> future1.complete(true));
-//        map
-//                .subscribe(e -> System.out.println(e+""), e -> e.printStackTrace(), () -> future2.complete(true));
-
-        System.out.println("Waiting for future 1");
-        future1.join();
-        System.out.println("Waiting for future 2");
-        future2.join();
-        BackupStreams.shutdown();
+        return future1.thenCompose(f -> future2);
     }
 
-    private static Integer intenseCalculation(Integer i) {
-        System.out.println("Hello from " + i);
-        return i;
-    }
-}
-//
-//class State {
-//    public File file;
-//    public ByteBuffer byteBuffer;
-//    public int number;
-//    public byte[] hash;
-//
-//    @Override
-//    public String toString() {
-//        return "State{" +
-//                ", number=" + number +
-//                ", hash=" + Arrays.toString(hash) +
-//                '}';
-//    }
-//}
-
-class LoggingSubscriber<T> implements Subscriber<T> {
-
-    @Override
-    public void onSubscribe(Subscription s) {
-
-    }
-
-    @Override
-    public void onNext(T o) {
-        System.out.println(o);
-    }
-
-    @Override
-    public void onError(Throwable t) {
-
-    }
-
-    @Override
-    public void onComplete() {
-
-    }
 }
